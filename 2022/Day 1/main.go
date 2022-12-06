cd2b3f5d9dee5bdacb0cd4f0a1f5a8fef95b4989
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-12-07 00:02:14 +0100

    Finish Part 1 of Day 1 🎄

--- 2022/Day 1/main.go
+++ 2022/Day 1/main.go
@@ -1,7 +1,43 @@
+// Exercise: https://adventofcode.com/2022/day/1
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"sort"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello Day 1!")
+	// read input
+	rawFile, err := ioutil.ReadFile("input")
+	if err != nil {
+		log.Panicln(err)
+	}
+	file := string(rawFile)
+
+	// process input
+	elfs := strings.Split(file, "\n\n")
+	var elfSums []int
+	for _, elf := range elfs {
+		calories := strings.Split(elf, "\n")
+		sum := 0
+		for _, calorie := range calories {
+			// Parse string to int
+			if len(calorie) > 0 {
+				i, err := strconv.Atoi(calorie)
+				if err != nil {
+					log.Fatalln("Error while converting input value string to int!" + calorie)
+				}
+				sum += i
+			}
+		}
+		elfSums = append(elfSums, sum)
+	}
+	// Sort elf and Print result
+	sort.Ints(elfSums)
+	fmt.Printf("Elf with most food carries %v calories.", elfSums[len(elfSums)-1])
 }
