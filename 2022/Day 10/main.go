034de87a37028544e2368d32640b89e17743746b
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-12-11 01:04:41 +0100

    Advent of Code Day 10 Part 1 🖥️

---
+++ 2022/Day 10/main.go
@@ -0,0 +1,54 @@
+// Exercise: https://adventofcode.com/2022/day/10
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func readInput(file string) string {
+	// read input
+	rawFile, err := ioutil.ReadFile("input")
+	if err != nil {
+		log.Panicln(err)
+	}
+	return string(rawFile)
+}
+
+func main() {
+	input := readInput("input.txt")
+	instructions := strings.Split(input, "\n")
+
+	cycles := make(map[int]int)
+
+	cycle := 0
+	x := 1
+
+	for _, i := range instructions {
+		if len(i) > 0 {
+			if i == "noop" {
+				cycle++
+				cycles[cycle] = x
+			} else {
+				cycles[cycle+1] = x
+				cycles[cycle+2] = x
+				cycle += 2
+				// get value from addx instruction
+				value, _ := strconv.Atoi(strings.Split(i, " ")[1])
+				x += value
+			}
+		}
+	}
+
+	signalStrengthSum := 0
+
+	for i := 20; i <= 220; i += 40 {
+		signalStrengthSum += i * cycles[i]
+	}
+
+	fmt.Printf("The sum of all signal strengths is %v\n", signalStrengthSum)
+}
