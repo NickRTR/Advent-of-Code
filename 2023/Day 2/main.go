8c07dc56e904a42a25ab14d1a06f8df360625dbf
Nick <95316598+NickRTR@users.noreply.github.com>
2023-12-02 23:45:10 +0100

    Advent of Code Day 2 Part 2 🌨️

--- 2023/Day 2/main.go
+++ 2023/Day 2/main.go
@@ -18,31 +18,45 @@ func readInput(file string) string {
 	return string(rawFile)
 }
 
+func getCount(line string) map[string]int {
+	values := strings.Split(line, ":")
+	count := map[string]int{
+		"red":   0,
+		"green": 0,
+		"blue":  0,
+	}
+	sets := strings.Split(values[1], ";")
+	for _, set := range sets {
+		items := strings.Split(set, ",")
+		for _, item := range items {
+			value := strings.Split(item, " ")
+			number, _ := strconv.ParseFloat(value[1], 64)
+			count[value[2]] = int(math.Max(float64(count[value[2]]), number))
+		}
+	}
+	return count
+}
+
 func part1(lines []string) int {
 	possibleGames := 0
 
-	for _, line := range lines {
-		values := strings.Split(line, ":")
-		game, _ := strconv.Atoi(strings.Split(values[0], " ")[1])
-		count := map[string]int{
-			"red":   0,
-			"green": 0,
-			"blue":  0,
-		}
-		sets := strings.Split(values[1], ";")
-		for _, set := range sets {
-			items := strings.Split(set, ",")
-			for _, item := range items {
-				value := strings.Split(item, " ")
-				number, _ := strconv.ParseFloat(value[1], 64)
-				count[value[2]] = int(math.Max(float64(count[value[2]]), number))
-			}
-		}
+	for index, line := range lines {
+		count := getCount(line)
+
 		if count["red"] <= 12 && count["green"] <= 13 && count["blue"] <= 14 {
-			possibleGames += game
+			possibleGames += index
 		}
 	}
 	return possibleGames
+}
+
+func part2(lines []string) int {
+	sum := 0
+	for _, line := range lines {
+		count := getCount(line)
+		sum += count["red"] * count["green"] * count["blue"]
+	}
+	return sum
 }
 
 func main() {
@@ -50,5 +64,6 @@ func main() {
 
 	lines := strings.Split(file, "\n")
 
-	println("The sum of the indexes of all possible games is:", part1(lines))
+	println("Part 1: The sum of the indexes of all possible games is:", part1(lines))
+	println("Part 2: The sum of the power of the minimum set of cubes for each games is:", part2(lines))
 }
