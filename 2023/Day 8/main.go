dfd208ab064ec126f9d46dd49abb0f9c5b6980e7
Nick <95316598+NickRTR@users.noreply.github.com>
2023-12-08 20:39:38 +0100

    Advent of Code Day 8 Part 2 👨‍🎓

--- 2023/Day 8/main.go
+++ 2023/Day 8/main.go
@@ -34,6 +34,55 @@ func part1(nodes map[string][]string, instructions []string) int {
 	return steps
 }
 
+// Helper function to calculate Greatest Common Divisor (GCD)
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+// Helper function to calculate Least Common Multiple (LCM)
+func lcm(a, b int) int {
+	return a * b / gcd(a, b)
+}
+
+func part2(nodes map[string][]string, instructions []string) int {
+	points := []string{}
+
+	for key := range nodes {
+		if string(key[2]) == "A" {
+			points = append(points, key)
+		}
+	}
+
+	results := []int{}
+
+	for _, value := range points {
+		steps := 0
+		current := value
+		for i := 0; string(current[2]) != "Z"; i++ {
+			steps += 1
+			if i == len(instructions) {
+				i = 0
+			}
+			if instructions[i] == "L" {
+				current = nodes[current][0]
+			} else {
+				current = nodes[current][1]
+			}
+		}
+		results = append(results, steps)
+	}
+
+	lcmValue := results[0]
+	for _, value := range results[1:] {
+		lcmValue = lcm(lcmValue, value)
+	}
+
+	return lcmValue
+}
+
 func main() {
 	file := readInput("input")
 
@@ -51,4 +100,5 @@ func main() {
 	}
 
 	fmt.Println("Part 1: Required steps to reach the target ZZZ:", part1(nodes, instructions))
+	fmt.Println("Part 2: Required steps to have all nodes end on a Z:", part2(nodes, instructions))
 }
